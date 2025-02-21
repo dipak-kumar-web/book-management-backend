3a88070ea67c4aa3790009b102ec1dd7059d0642
Dipak Kumar <dipakkumar.web@gmail.com>
2025-02-21 15:30:41 +0530

    fixed cors policy

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(ratelimiter);
 // CORS configuration for cookies
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
